d97b5a6f9c1b9d20960e4365de83677b97fb4fcf
Fredy Navarro <navarro.fredy@gmail.com>
2024-09-23 21:45:19 -0300

    Cambios fin de Agosto

--- src/components/Cotizador.js
+++ src/components/Cotizador.js
@@ -9,6 +9,7 @@ import {
   orderBy,
   limit,
 } from "firebase/firestore";
+import NavBar from "./NavBar"; // Importamos el NavBar
 
 function Cotizador() {
   const [clienteData, setClienteData] = useState({
@@ -23,18 +24,34 @@ function Cotizador() {
   });
 
   const [productos, setProductos] = useState([]);
+  const [materiales, setMateriales] = useState({});
   const [items, setItems] = useState([]);
   const [totalCotizacion, setTotalCotizacion] = useState(0);
 
   // Función para obtener los productos desde Firestore
   const fetchProductos = async () => {
     const productosRef = await getDocs(collection(db, "productos"));
-    setProductos(
-      productosRef.docs.map((doc) => ({
+    const productosList = productosRef.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+
+    setProductos(productosList);
+
+    // Para cada producto, obtener sus materiales y almacenarlos en un objeto separado
+    for (const producto of productosList) {
+      const materialesRef = await getDocs(
+        collection(db, "productos", producto.id, "materiales")
+      );
+      const materialesList = materialesRef.docs.map((doc) => ({
         id: doc.id,
         ...doc.data(),
-      }))
-    );
+      }));
+      setMateriales((prevState) => ({
+        ...prevState,
+        [producto.id]: materialesList,
+      }));
+    }
   };
 
   useEffect(() => {
@@ -50,9 +67,13 @@ function Cotizador() {
       limit(1)
     );
     const cotizacionesSnapshot = await getDocs(cotizacionesQuery);
+
+    // Si no hay cotizaciones, comenzamos en 1
     if (cotizacionesSnapshot.empty) {
       return 1;
     }
+
+    // Obtener el último número de cotización
     const ultimoNumeroCotizacion =
       cotizacionesSnapshot.docs[0].data().numeroCotizacion;
     return ultimoNumeroCotizacion + 1;
@@ -65,8 +86,8 @@ function Cotizador() {
       {
         id: items.length + 1,
         localizacion: "",
-        producto: null,
-        material: null,
+        productoId: "",
+        materialId: "",
         motorizado: false,
         doble: false,
         cenefa: false,
@@ -86,10 +107,15 @@ function Cotizador() {
       if (item.id === id) {
         item[field] = value;
 
+        // Si se cambia el producto, restablecer el material
+        if (field === "productoId") {
+          item.materialId = ""; // Restablecer material cuando se cambia el producto
+        }
+
         // Si se cambian ancho, largo o producto, recalculamos el total
-        if (field === "ancho" || field === "largo" || field === "producto") {
+        if (field === "ancho" || field === "largo" || field === "productoId") {
           const productoSeleccionado = productos.find(
-            (prod) => prod.id === item.producto
+            (prod) => prod.id === item.productoId
           );
           if (productoSeleccionado) {
             item.total =
@@ -130,224 +156,249 @@ function Cotizador() {
   };
 
   return (
-    <div className="p-6">
-      <h2 className="text-2xl mb-4">Cotizador</h2>
+    <div>
+      <NavBar /> {/* Aquí se agrega el NavBar */}
+      <div className="p-6">
+        <h2 className="text-2xl mb-4">Cotizador</h2>
 
-      {/* Sección Datos del Cliente */}
-      <div className="mb-6">
-        <h3 className="text-xl">Datos del Cliente</h3>
-        <input
-          type="text"
-          placeholder="Nombre del Proyecto"
-          value={clienteData.nombreProyecto}
-          onChange={(e) =>
-            setClienteData({ ...clienteData, nombreProyecto: e.target.value })
-          }
-          className="border p-2 w-full mb-2"
-        />
-        <input
-          type="text"
-          placeholder="Nombre del Cliente"
-          value={clienteData.nombreCliente}
-          onChange={(e) =>
-            setClienteData({ ...clienteData, nombreCliente: e.target.value })
-          }
-          className="border p-2 w-full mb-2"
-        />
-        <input
-          type="text"
-          placeholder="Ciudad"
-          value={clienteData.ciudad}
-          onChange={(e) =>
-            setClienteData({ ...clienteData, ciudad: e.target.value })
-          }
-          className="border p-2 w-full mb-2"
-        />
-        <input
-          type="text"
-          placeholder="Comuna"
-          value={clienteData.comuna}
-          onChange={(e) =>
-            setClienteData({ ...clienteData, comuna: e.target.value })
-          }
-          className="border p-2 w-full mb-2"
-        />
-        <input
-          type="email"
-          placeholder="Correo electrónico"
-          value={clienteData.correo}
-          onChange={(e) =>
-            setClienteData({ ...clienteData, correo: e.target.value })
-          }
-          className="border p-2 w-full mb-2"
-        />
-        <input
-          type="text"
-          placeholder="Teléfono"
-          value={clienteData.telefono}
-          onChange={(e) =>
-            setClienteData({ ...clienteData, telefono: e.target.value })
-          }
-          className="border p-2 w-full mb-2"
-        />
-        <input
-          type="text"
-          placeholder="Dirección"
-          value={clienteData.direccion}
-          onChange={(e) =>
-            setClienteData({ ...clienteData, direccion: e.target.value })
-          }
-          className="border p-2 w-full mb-2"
-        />
-        <textarea
-          placeholder="Observaciones"
-          value={clienteData.observacion}
-          onChange={(e) =>
-            setClienteData({ ...clienteData, observacion: e.target.value })
-          }
-          className="border p-2 w-full mb-2"
-        />
-      </div>
+        {/* Sección Datos del Cliente */}
+        <div className="mb-6">
+          <h3 className="text-xl">Datos del Cliente</h3>
+          <input
+            type="text"
+            placeholder="Nombre del Proyecto"
+            value={clienteData.nombreProyecto}
+            onChange={(e) =>
+              setClienteData({ ...clienteData, nombreProyecto: e.target.value })
+            }
+            className="border p-2 w-full mb-2"
+          />
+          <input
+            type="text"
+            placeholder="Nombre del Cliente"
+            value={clienteData.nombreCliente}
+            onChange={(e) =>
+              setClienteData({ ...clienteData, nombreCliente: e.target.value })
+            }
+            className="border p-2 w-full mb-2"
+          />
+          <input
+            type="text"
+            placeholder="Ciudad"
+            value={clienteData.ciudad}
+            onChange={(e) =>
+              setClienteData({ ...clienteData, ciudad: e.target.value })
+            }
+            className="border p-2 w-full mb-2"
+          />
+          <input
+            type="text"
+            placeholder="Comuna"
+            value={clienteData.comuna}
+            onChange={(e) =>
+              setClienteData({ ...clienteData, comuna: e.target.value })
+            }
+            className="border p-2 w-full mb-2"
+          />
+          <input
+            type="email"
+            placeholder="Correo electrónico"
+            value={clienteData.correo}
+            onChange={(e) =>
+              setClienteData({ ...clienteData, correo: e.target.value })
+            }
+            className="border p-2 w-full mb-2"
+          />
+          <input
+            type="text"
+            placeholder="Teléfono"
+            value={clienteData.telefono}
+            onChange={(e) =>
+              setClienteData({ ...clienteData, telefono: e.target.value })
+            }
+            className="border p-2 w-full mb-2"
+          />
+          <input
+            type="text"
+            placeholder="Dirección"
+            value={clienteData.direccion}
+            onChange={(e) =>
+              setClienteData({ ...clienteData, direccion: e.target.value })
+            }
+            className="border p-2 w-full mb-2"
+          />
+          <textarea
+            placeholder="Observaciones"
+            value={clienteData.observacion}
+            onChange={(e) =>
+              setClienteData({ ...clienteData, observacion: e.target.value })
+            }
+            className="border p-2 w-full mb-2"
+          />
+        </div>
 
-      {/* Sección Cotización */}
-      <div>
-        <h3 className="text-xl mb-4">Cotización</h3>
+        {/* Sección Cotización */}
+        <div>
+          <h3 className="text-xl mb-4">Cotización</h3>
 
-        {items.map((item) => (
-          <div
-            key={item.id}
-            className="mb-4 border p-4"
-          >
-            <h4 className="font-bold">Item {item.id}</h4>
-
-            <input
-              type="text"
-              placeholder="Localización"
-              value={item.localizacion}
-              onChange={(e) =>
-                handleItemChange(item.id, "localizacion", e.target.value)
-              }
-              className="border p-2 w-full mb-2"
-            />
-
-            <select
-              value={item.producto}
-              onChange={(e) =>
-                handleItemChange(item.id, "producto", e.target.value)
-              }
-              className="border p-2 w-full mb-2"
+          {items.map((item) => (
+            <div
+              key={item.id}
+              className="mb-4 border p-4"
             >
-              <option value="">Seleccionar Producto</option>
-              {productos.map((producto) => (
-                <option
-                  key={producto.id}
-                  value={producto.id}
+              <h4 className="font-bold">Item {item.id}</h4>
+
+              <input
+                type="text"
+                placeholder="Localización"
+                value={item.localizacion}
+                onChange={(e) =>
+                  handleItemChange(item.id, "localizacion", e.target.value)
+                }
+                className="border p-2 w-full mb-2"
+              />
+
+              {/* Selección de producto */}
+              <select
+                value={item.productoId}
+                onChange={(e) =>
+                  handleItemChange(item.id, "productoId", e.target.value)
+                }
+                className="border p-2 w-full mb-2"
+              >
+                <option value="">Seleccionar Producto</option>
+                {productos.map((producto) => (
+                  <option
+                    key={producto.id}
+                    value={producto.id}
+                  >
+                    {producto.name} - ${producto.price}
+                  </option>
+                ))}
+              </select>
+
+              {/* Selección de material basado en producto */}
+              {item.productoId && (
+                <select
+                  value={item.materialId}
+                  onChange={(e) =>
+                    handleItemChange(item.id, "materialId", e.target.value)
+                  }
+                  className="border p-2 w-full mb-2"
                 >
-                  {producto.name} - ${producto.price}
-                </option>
-              ))}
-            </select>
+                  <option value="">Seleccionar Material</option>
+                  {materiales[item.productoId]?.map((material) => (
+                    <option
+                      key={material.id}
+                      value={material.id}
+                    >
+                      {material.name}
+                    </option>
+                  ))}
+                </select>
+              )}
 
-            {/* Checkbox y Selects para otras opciones */}
-            <input
-              type="checkbox"
-              checked={item.motorizado}
-              onChange={(e) =>
-                handleItemChange(item.id, "motorizado", e.target.checked)
-              }
-            />
-            <label> Motorizado </label>
+              {/* Checkbox y Selects para otras opciones */}
+              <input
+                type="checkbox"
+                checked={item.motorizado}
+                onChange={(e) =>
+                  handleItemChange(item.id, "motorizado", e.target.checked)
+                }
+              />
+              <label> Motorizado </label>
 
-            <input
-              type="checkbox"
-              checked={item.doble}
-              onChange={(e) =>
-                handleItemChange(item.id, "doble", e.target.checked)
-              }
-            />
-            <label> Doble </label>
+              <input
+                type="checkbox"
+                checked={item.doble}
+                onChange={(e) =>
+                  handleItemChange(item.id, "doble", e.target.checked)
+                }
+              />
+              <label> Doble </label>
 
-            <input
-              type="checkbox"
-              checked={item.cenefa}
-              onChange={(e) =>
-                handleItemChange(item.id, "cenefa", e.target.checked)
-              }
-            />
-            <label> Cenefa </label>
+              <input
+                type="checkbox"
+                checked={item.cenefa}
+                onChange={(e) =>
+                  handleItemChange(item.id, "cenefa", e.target.checked)
+                }
+              />
+              <label> Cenefa </label>
 
-            <input
-              type="checkbox"
-              checked={item.cadenaMetalica}
-              onChange={(e) =>
-                handleItemChange(item.id, "cadenaMetalica", e.target.checked)
-              }
-            />
-            <label> Cadena metálica </label>
+              <input
+                type="checkbox"
+                checked={item.cadenaMetalica}
+                onChange={(e) =>
+                  handleItemChange(item.id, "cadenaMetalica", e.target.checked)
+                }
+              />
+              <label> Cadena metálica </label>
 
-            <select
-              value={item.colocacion}
-              onChange={(e) =>
-                handleItemChange(item.id, "colocacion", e.target.value)
-              }
-              className="border p-2 w-full mb-2"
-            >
-              <option value="oculta">Oculta</option>
-              <option value="a_la_vista">A la vista</option>
-            </select>
+              <select
+                value={item.colocacion}
+                onChange={(e) =>
+                  handleItemChange(item.id, "colocacion", e.target.value)
+                }
+                className="border p-2 w-full mb-2"
+              >
+                <option value="oculta">Oculta</option>
+                <option value="a_la_vista">A la vista</option>
+              </select>
 
-            <select
-              value={item.colorAnclaje}
-              onChange={(e) =>
-                handleItemChange(item.id, "colorAnclaje", e.target.value)
-              }
-              className="border p-2 w-full mb-2"
-            >
-              <option value="blanco">Blanco</option>
-              <option value="negro">Negro</option>
-              <option value="beige">Beige</option>
-              <option value="gris">Gris</option>
-            </select>
+              <select
+                value={item.colorAnclaje}
+                onChange={(e) =>
+                  handleItemChange(item.id, "colorAnclaje", e.target.value)
+                }
+                className="border p-2 w-full mb-2"
+              >
+                <option value="blanco">Blanco</option>
+                <option value="negro">Negro</option>
+                <option value="beige">Beige</option>
+                <option value="gris">Gris</option>
+              </select>
 
-            <input
-              type="number"
-              placeholder="Ancho"
-              value={item.ancho}
-              onChange={(e) =>
-                handleItemChange(item.id, "ancho", e.target.value)
-              }
-              className="border p-2 w-full mb-2"
-            />
-            <input
-              type="number"
-              placeholder="Largo"
-              value={item.largo}
-              onChange={(e) =>
-                handleItemChange(item.id, "largo", e.target.value)
-              }
-              className="border p-2 w-full mb-2"
-            />
-            <p>Total Item: ${item.total.toFixed(2)}</p>
-          </div>
-        ))}
+              <input
+                type="number"
+                placeholder="Ancho"
+                value={item.ancho}
+                onChange={(e) =>
+                  handleItemChange(item.id, "ancho", e.target.value)
+                }
+                className="border p-2 w-full mb-2"
+              />
+              <input
+                type="number"
+                placeholder="Largo"
+                value={item.largo}
+                onChange={(e) =>
+                  handleItemChange(item.id, "largo", e.target.value)
+                }
+                className="border p-2 w-full mb-2"
+              />
+              <p>Total Item: ${item.total.toFixed(2)}</p>
+            </div>
+          ))}
 
-        <button
-          onClick={handleAddItem}
-          className="bg-blue-500 text-white px-4 py-2 rounded"
-        >
-          Agregar Producto
-        </button>
+          <button
+            onClick={handleAddItem}
+            className="bg-blue-500 text-white px-4 py-2 rounded"
+          >
+            Agregar Producto
+          </button>
 
-        <h3 className="mt-6 font-bold">
-          Total Cotización: ${totalCotizacion.toFixed(2)}
-        </h3>
+          <h3 className="mt-6 font-bold">
+            Total Cotización: ${totalCotizacion.toFixed(2)}
+          </h3>
 
-        <button
-          onClick={handleSaveCotizacion}
-          className="bg-green-500 text-white px-4 py-2 mt-4 rounded"
-        >
-          Guardar Cotización
-        </button>
+          <button
+            onClick={handleSaveCotizacion}
+            className="bg-green-500 text-white px-4 py-2 mt-4 rounded"
+          >
+            Guardar Cotización
+          </button>
+        </div>
       </div>
     </div>
   );
